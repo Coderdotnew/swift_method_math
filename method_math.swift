387b3ba690d20218838ab53770f04e1fe4aeab53
Stephanholly <sholly@usc.edu>
2016-10-19 11:39:18 -0700

    challenge 3

---
+++ method_math.swift
@@ -0,0 +1,16 @@
+// Write your code below! NOTE: you may need to look up the last two. Remember: Google is your friend!
+
+
+//Create a function that takes in two integer arguments and adds them together when the function is called.
+
+//Create a function that takes in two integer arguments and subtracts them from eachother when the function is called.
+
+//Create a function that takes in two integer arguments and multiplies them together when the function is called.
+
+//Create a function that takes in two integer arguments and divides the integers when the function is called.
+
+//Create a function that takes in two integer arguments and finds the remainder when the first number is divided by the second number when the function is called.
+
+//Create a function that takes in two integer arguments (base and exponent) that creates an exponential equation the function is called. (i.e. base ^ exponent)
+
+//Create a function that takes in one integer argument and finds the square root when the function is called.
